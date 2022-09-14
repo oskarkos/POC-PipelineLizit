0787f55b0ceb2b2eae8279cab2a4de47ef879847
Oscar Arcos <os-da-ar@hotmail.com>
2022-09-14 14:10:08 -0500

    env variables

--- src/App.tsx
+++ src/App.tsx
@@ -15,7 +15,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Prueba implementacion kjfhksdjhfkshfks</h1>
+      <h1>Prueba implementacion {import.meta.env.VITE_SOME_KEY}</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
