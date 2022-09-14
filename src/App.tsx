1bd7467b40c2eb2d1226cfbbc7625533715e96aa
Oscar Arcos <os-da-ar@hotmail.com>
2022-09-14 10:09:35 -0500

    prueba

--- src/App.tsx
+++ src/App.tsx
@@ -15,7 +15,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Prueba implementacion</h1>
+      <h1>Prueba implementacion kjfhksdjhfkshfks</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
